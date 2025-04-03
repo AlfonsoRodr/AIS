1cb92721d731ad91a461f9909a514ba0dca29f18
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-04-03 23:41:23 +0200

    Add Fisrts Version of Test

--- CalculatorParser/src/main/java/es/codeurjc/test/CalculatorParser.java
+++ CalculatorParser/src/main/java/es/codeurjc/test/CalculatorParser.java
@@ -2,12 +2,34 @@ package es.codeurjc.test;
 
 public class CalculatorParser {
 
+    /**
+     * Method that verifies if a given expression contains a letter.
+     * @param expression said expression.
+     * @return true if it does contains a letter, false otherwise.
+     */
+    private boolean containsALetter(String expression) {
+        for (char c : expression.toCharArray()) {
+            if (Character.isLetter(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int parse(String expression) {
         if ((expression.length() == 1) && (Character.isLetter(expression.charAt(0)))) {
             throw new NumberFormatException("The expression received is not a number.");
         }
-        else {
-            return Integer.parseInt(expression);
+        else if ((expression.length() > 1) && (this.containsALetter(expression))) {
+            throw new NumberFormatException("The expression received is not a number.");
         }
-    }	
+        else {
+            String[] numbers = expression.split("\\+");
+            int sum = 0;
+            for (String num : numbers) {
+                sum = sum + Integer.parseInt(num.trim()); // Delete blank spaces.
+            }
+            return sum;
+        }
+    }
 }
--- CalculatorParser/src/test/java/es/codeurjc/test/CalculatorParserTest.java
+++ CalculatorParser/src/test/java/es/codeurjc/test/CalculatorParserTest.java
@@ -26,8 +26,45 @@ public class CalculatorParserTest {
 
     @Test
     @DisplayName("When receiving an expression with a letter, a NumberFormatException should be thrown")
-    public void expressionWithLetterTest() {
+    public void singleLetterTest() {
         String operation = "A";
+        NumberFormatException thrown = assertThrows(NumberFormatException.class, () -> {
+            this.calculator.parse(operation);
+        });
+        assertEquals(ERROR_MESSAGE, thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("When receiving an expression with multiple letters, a NumberFormatException should be thrown")
+    public void multipleLettersTest() {
+        String operation = "Hello";
+        NumberFormatException thrown = assertThrows(NumberFormatException.class, () -> {
+            this.calculator.parse(operation);
+        });
+        assertEquals(ERROR_MESSAGE, thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("When receiving an expression that mixes numbers and letters, a NumberFormatException should be thrown.")
+    public void mixNumbersAndLettersTest() {
+        String operation = "19283939AB726X6";
+        NumberFormatException thrown = assertThrows(NumberFormatException.class, () -> {
+            this.calculator.parse(operation);
+        });
+        assertEquals(ERROR_MESSAGE, thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("When receiving an arithmethic sum, the result of said sum should be returned")
+    public void arithmeticSumTest() {
+        String operation = "5+3+6+8";
+        assertEquals(22, this.calculator.parse(operation));
+    }
+
+    @Test
+    @DisplayName("When receiving an invalid format for an arithmethic sum, the result of said sum should be returned")
+    public void invalidAritmethicSumTest() {
+        String operation = "4+3+A+7+C+88+BB";
         NumberFormatException thrown = assertThrows(NumberFormatException.class, () -> {
             this.calculator.parse(operation);
         });
